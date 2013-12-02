8b930014076db9d3fad526d5f86fd06142270030
calebdel <delbegio@gmail.com>
2013-12-02 10:49:10 -0800

    initial commit with postgres db and foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microaccel::Application.config.secret_key_base = '22302d1960dd5db16b16b79f355fbfffb32a9e021fddcd19f8accf29159c62294a8708896ea2cbfb038c90cf3a4280ebc08cb788d49a95c5d13a878fd963d414'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Microaccel::Application.config.session_store :cookie_store, key: '_microaccel_session'
